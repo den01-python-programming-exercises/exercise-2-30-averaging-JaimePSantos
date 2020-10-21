e11fe09a5e8a571c3bacb06ab279bc219a7bbadf
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-21 19:54:37 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
+def sum(number1, number2, number3, number4):
+    # you can copy your implementation of the method sum here
+    soma= number1+ number2+ number3+ number4
+    return soma
+def average(number1, number2, number3, number4):
+    # write your code here
+    # calculate the sum of the elements by calling the method sum
+    avg = sum(number1, number2, number3, number4) /4
+    return avg
+ 
 def main():
-    #write your code below this line
+    result = average(4, 3, 6, 1)
+    print("Average: " + str(result))
 
 if __name__ == '__main__':
     main()
